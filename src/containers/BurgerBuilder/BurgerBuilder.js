2961194644f0dc9876915b512a02532a0b09c0c3
Abhishek Patharia <warmachine7750@gmail.com>
2020-07-30 21:22:46 +0530

    Update: Added Routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,28 +93,17 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true});
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: 'Abhishek Patharia',
-      address: {
-        street: 'TestStreet1',
-        zipCode: '560089',
-        country: 'India'
-      },
-      email: 'test@testmail.com',
-      deliveryMethod: 'fastest'
+    const queryParams = [];
+    for(let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i)+"="+encodeURIComponent(this.state.ingredients[i]));
     }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-        console.log(response);
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-        console.log(error);
-      });
+    queryParams.push('price='+this.state.totalPrice);
+    const queryString = queryParams.join('&');
+
+    this.props.history.push({
+      pathname: "/checkout",
+      search: '?' + queryString
+    });
   };
 
   render() {
